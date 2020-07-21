a8ca766164b5ad43e1f29fc20192cd4b22dae437
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-21 13:32:41 +0200

    Implemented basic logic for deregistering a sub

--- lambdas/deregister-sub/deregister-sub.js
+++ lambdas/deregister-sub/deregister-sub.js
@@ -1,13 +1,23 @@
-const getJwtUser = require('get-jwt-user')
+const { getJwtUser } = require('push-api-utils')
+const removeSub = require('./helpers/remove-sub.js')
 
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
+  var res = {
+    statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      message: getJwtUser()
-    })
+    }
   }
+  return await removeSub(getJwtUser(event.headers.Authorization), JSON.parse(event.body))
+    .then(esRes => {
+      res.body = JSON.stringify({ message: 'Subscriptions successfully removed!' })
+      console.log(JSON.stringify(esRes, null, 2))
+      return res
+    })
+    .catch(err => {
+      res.statusCode = 400
+      res.body = JSON.stringify({ message: 'An error occured while removing the subscription...' })
+      console.log(JSON.stringify(err, null, 2))
+      return res
+    })
 }
