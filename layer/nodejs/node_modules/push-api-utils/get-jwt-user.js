5b9ec9e2c03fe983918c079bae6c41bf1d1c8b94
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-22 12:29:40 +0200

    Cleanup: removed old utils

--- layer/nodejs/node_modules/push-api-utils/get-jwt-user.js
+++
@@ -1,4 +0,0 @@
-module.exports = (authorizationHeader) => {
-  const jwt = authorizationHeader.split(' ')[1]
-  return JSON.parse(Buffer.from(jwt.split('.')[1], 'base64').toString()).email
-}
--- layer/nodejs/node_modules/push-api-utils/get-script.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sub, op) => {
-  return {
-    lang: 'painless',
-    source: op === 'deregister' ? 'ctx._source.subscriptions.remove(ctx._source.subscriptions.indexOf(params.sub))' : 'ctx._source.subscriptions.add(params.sub)',
-    params: {
-      sub: sub
-    }
-  }
-}
