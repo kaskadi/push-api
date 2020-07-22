b279e567d32e050124989f5261b11a8e479b9930
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-22 12:28:01 +0200

    Hotfix: updated logic for layer utils installation

---
+++ layer/nodejs/push-api-utils/main.js
@@ -0,0 +1,6 @@
+module.exports = {
+  getJwtUser: require('./get-jwt-user.js'),
+  esUpdateByQuery: require('./es-update-by-query.js'),
+  getScript: require('./get-script.js'),
+  getQuery: require('./get-query.js')
+}
