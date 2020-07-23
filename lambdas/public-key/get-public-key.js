990e887d6d19f157cd1c64d49817db8229606883
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-23 17:37:01 +0200

    Hotfix: fixed typo (path to utility)

--- lambdas/public-key/get-public-key.js
+++ lambdas/public-key/get-public-key.js
@@ -1,5 +1,5 @@
 const getKeys = require('./helpers/get-keys.js')
-const buildResponse = require('./helpers/get-keys.js')
+const buildResponse = require('./helpers/build-response.js')
 
 module.exports.handler = async (event) => {
   return await getKeys().then(buildResponse)
